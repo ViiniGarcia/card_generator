64e00c19b295ffd8738ec93c816b8008b88014cd
ViiniGarcia <viniciusgrlopes@hotmail.com>
2024-07-20 14:20:13 -0300

    Update encontristas card & name Abbreviation

--- lib/Utils/functions.dart
+++ lib/Utils/functions.dart
@@ -8,7 +8,7 @@ import 'package:pdf/pdf.dart';
 import 'package:pdf/widgets.dart' as pw;
 import 'package:printing/printing.dart';
 
-Future<List<BadgeEJC>> pickerExcelFile() async {
+Future<List<BadgeEJC>> pickerExcelFile(TypeBadge? typeBadge) async {
   FilePickerResult? pickedFile = await FilePicker.platform.pickFiles(
     type: FileType.custom,
     allowedExtensions: ['xlsx'],
@@ -16,19 +16,30 @@ Future<List<BadgeEJC>> pickerExcelFile() async {
   );
 
   Set<BadgeEJC> listBadges = {};
-  
   if (pickedFile != null) {
-    Uint8List? bytes = pickedFile.files.single.bytes;
-    Excel excel = Excel.decodeBytes(bytes!);
-    Sheet? table = excel.tables.values.first;
-    int? qntRows = table.maxRows;
-
-    for (int x = 1; x<=(qntRows - 1); x++){
-      var squad = table.row(x).elementAt(1)?.value.toString().removeSpecialCaracters();
-      var name = table.row(x).elementAt(2)?.value.toString().split(" ").nameCaptalize();
-      var nickname = table.row(x).elementAt(3)?.value.toString().toUpperCase();
-      listBadges.add(BadgeEJC(name: name!, nickname: nickname!, squad: squad!));
-    }
+    try{
+      Uint8List bytes = pickedFile.files.single.bytes!;
+      Excel excel = Excel.decodeBytes(bytes);
+      Sheet? table = excel.tables.values.first;
+      int? qntRows = table.maxRows;
+      if(typeBadge == TypeBadge.encontreiro){
+        for (int x = 1; x<=(qntRows - 1); x++){
+          var squad = table.row(x).elementAt(1)?.value.toString().removeSpecialCaracters();
+          var name = table.row(x).elementAt(2)?.value.toString().toUpperCase().nameAbbreviation();
+          var nickname = table.row(x).elementAt(3)?.value.toString().toUpperCase();
+          listBadges.add(BadgeEJC(name: name!, nickname: nickname!, squad: squad!));
+        }
+      }else{
+        for (int x = 1; x<=(qntRows - 1); x++){
+          var squad = table.row(x).elementAt(5)?.value.toString().removeSpecialCaracters();
+          var name = table.row(x).elementAt(0)?.value.toString().toUpperCase().nameAbbreviation();
+          var nickname = table.row(x).elementAt(2)?.value.toString().toUpperCase();
+          listBadges.add(BadgeEJC(name: name!, nickname: nickname!, squad: squad!));
+        }
+      }
+    }catch(e){
+      print(e);
+    };
   }
 
   return listBadges.toList();
@@ -36,19 +47,16 @@ Future<List<BadgeEJC>> pickerExcelFile() async {
 
 Future<void> printDoc(List<BadgeEJC> listBadges) async {
   final fontNick = await PdfGoogleFonts.robotoBlack();
-  final fontName = await PdfGoogleFonts.robotoLight();
+  final fontName = await PdfGoogleFonts.robotoMedium();
   final doc = pw.Document();
   Uint8List pdf;
   List<pw.Widget> badgesWidgets = [];
   
   for (var badgeInfo in listBadges) {
-    double fontSize = badgeInfo.nickname.length <= 10 ? 26 : 24;
-    //ByteData img = await rootBundle.load("assets/${badgeInfo.squad}.png");
-    //ByteData img = await rootBundle.load("assets/Regras.png");
-    //Uint8List imageBytes = img.buffer.asUint8List();
+    double fontSize = badgeInfo.nickname.length <= 10 ? 20 : 20;
     final image = await imageFromAssetBundle('assets/${badgeInfo.squad}.jpg');
     badgesWidgets.add(pw.Container(
-      padding: const pw.EdgeInsetsDirectional.fromSTEB(0, 0, 0, 62),
+      padding: const pw.EdgeInsetsDirectional.fromSTEB(0, 0, 0, 45),
       decoration: pw.BoxDecoration(
         image: pw.DecorationImage(
           image: pw.Image(image).image,
@@ -61,7 +69,7 @@ Future<void> printDoc(List<BadgeEJC> listBadges) async {
         children: [
           pw.Text(badgeInfo.nickname, style: pw.TextStyle(fontSize: fontSize, font: fontNick, color: PdfColor.fromHex('#fff'))),
           pw.SizedBox(height: 2),
-          pw.Text(badgeInfo.name, style: pw.TextStyle(fontSize: 12, font: fontName, color: PdfColor.fromHex('#fff'))),
+          pw.Text(badgeInfo.name, style: pw.TextStyle(fontSize: 8, fontBold: fontName, color: PdfColor.fromHex('#fff'))),
         ],
       ),
     ));
@@ -70,13 +78,14 @@ Future<void> printDoc(List<BadgeEJC> listBadges) async {
   doc.addPage(pw.MultiPage(
       maxPages: 200,
       pageFormat: PdfPageFormat.a4,
+      margin: const pw.EdgeInsets.all(10),
       build: (pw.Context context) {
         return [
           pw.GridView(
-            crossAxisCount: 2,
-            mainAxisSpacing: 10,
-            crossAxisSpacing: 10,
-            childAspectRatio: 1500 / 1000,
+            crossAxisCount: 3,
+            mainAxisSpacing: 3,
+            crossAxisSpacing: 3,
+            childAspectRatio: 1.4179,
             children: badgesWidgets,
           )
         ];
